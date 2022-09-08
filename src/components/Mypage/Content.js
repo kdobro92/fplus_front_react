adee80ab7dbf58aeed42f2dd15ab93092fb7432b
kdobro92 <kdobro92@gmail.com>
2022-09-08 15:24:23 +0900

    추가: mypage.js

--- src/components/Mypage/Content.js
+++ src/components/Mypage/Content.js
@@ -1,2 +1,16 @@
-function Content() {}
+import './Content.css';
+
+function Content() {
+  return (
+    <div className="wrap_mp_content">
+      <div className="mp_content_con">
+        <p className="mp_content_tit">포트폴리오</p>
+        <span className="mp_content_txt">
+          개성과 매력이 돋보이는 사진을 올려주세요!
+        </span>
+        <span className="mp_content_more">더보기</span>
+      </div>
+    </div>
+  );
+}
 export default Content;
