59500c5af948412d3ef23a3b919a8c54b8e18f2e
kdobro92 <kdobro92@gmail.com>
2022-09-08 14:34:10 +0900

    for commit

--- src/components/Mypage/Banner.js
+++ src/components/Mypage/Banner.js
@@ -4,9 +4,16 @@ function Banner() {
   return (
     <div className="banner_con">
       <img src="img/mypage_banner.png" alt="banner" />
-      <div className="aa">0</div>
-      <div className="bb">0</div>
-      <div className="bb">0</div>
+      <div className="abc">
+        <li className="aa">0</li>
+        <li className="aa">0</li>
+        <li className="aa">0</li>
+      </div>
+      <div className="abcd">
+        <li className="bb">포트폴리오</li>
+        <li className="bb">팔로워</li>
+        <li className="bb">오디션</li>
+      </div>
     </div>
   );
 }
