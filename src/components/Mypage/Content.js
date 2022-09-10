57bdf7a3b3e5d25875fafc0af7ecea6020119b2a
kdobro92 <kdobro92@gmail.com>
2022-09-10 16:32:01 +0900

    수정: mypage.js

--- src/components/Mypage/Content.js
+++ src/components/Mypage/Content.js
@@ -3,12 +3,30 @@ import './Content.css';
 function Content() {
   return (
     <div className="wrap_mp_content">
-      <div className="mp_content_con">
-        <p className="mp_content_tit">포트폴리오</p>
-        <span className="mp_content_txt">
-          개성과 매력이 돋보이는 사진을 올려주세요!
-        </span>
-        <span className="mp_content_more">더보기</span>
+      <div className="content_row_1">1</div>
+      <div className="content_row_2">
+        <div className="mp_content_con">
+          <p className="mp_content_tit">포트폴리오</p>
+          <span className="mp_content_txt">
+            개성과 매력이 돋보이는 사진을 올려주세요!
+          </span>
+          <span className="mp_content_more">더보기</span>
+        </div>
+        <div className="mp_content_img_con">
+          <div className="aa">
+            <input type="file" id="mp_file" />
+            <img src="img/upload_mp.png" alt="mp_img" />
+          </div>
+          <div className="aa">
+            <img src="img/upload_mp.png" alt="mp_img" />
+          </div>
+          <div className="aa">
+            <img src="img/upload_mp.png" alt="mp_img" />
+          </div>
+          <div className="aa">
+            <img src="img/upload_mp.png" alt="mp_img" />
+          </div>
+        </div>
       </div>
     </div>
   );
