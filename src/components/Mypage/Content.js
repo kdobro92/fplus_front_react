908ee2c81f558cdf102d1ff63f15da89222d9b65
kdobro92 <kdobro92@gmail.com>
2022-09-11 15:37:52 +0900

    수정: mypage.js

--- src/components/Mypage/Content.js
+++ src/components/Mypage/Content.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import './Content.css';
 
 function Content() {
@@ -10,21 +11,119 @@ function Content() {
           <span className="mp_content_txt">
             개성과 매력이 돋보이는 사진을 올려주세요!
           </span>
-          <span className="mp_content_more">더보기</span>
+          <div className="more_port_con">
+            <span className="more_more">더보기</span>
+            <img src="img/arrow_front.png" alt="more" className="more_img" />
+          </div>
         </div>
         <div className="mp_content_img_con">
-          <div className="aa">
-            <input type="file" id="mp_file" />
+          <div className="mp_upload">
+            <input
+              type="file"
+              className="mp_file"
+              src="img/upload_mp.png"
+              accept="image/jpg,image/png,image/jpeg"
+            />
             <img src="img/upload_mp.png" alt="mp_img" />
           </div>
-          <div className="aa">
+          <div className="mp_upload">
+            <input
+              type="file"
+              className="mp_file"
+              src="img/upload_mp.png"
+              accept="image/jpg,image/png,image/jpeg"
+            />
             <img src="img/upload_mp.png" alt="mp_img" />
           </div>
-          <div className="aa">
+          <div className="mp_upload">
+            <input
+              type="file"
+              className="mp_file"
+              src="img/upload_mp.png"
+              accept="image/jpg,image/png,image/jpeg"
+            />
             <img src="img/upload_mp.png" alt="mp_img" />
           </div>
-          <div className="aa">
+          <div className="mp_upload">
+            <input
+              type="file"
+              className="mp_file"
+              src="img/upload_mp.png"
+              accept="image/jpg,image/png,image/jpeg"
+            />
             <img src="img/upload_mp.png" alt="mp_img" />
+          </div>
+        </div>
+        <div className="user_content_con">
+          <div className="user_content_list">
+            <div className="user_mp">
+              <div className="user_mp_txt">
+                <p>참여/수상경력</p>
+                <span>예시) 2022년 영화'애드위즈' 출연</span>
+                <div className="more_con">
+                  <span className="more_more">더보기</span>
+                  <img
+                    src="img/arrow_front.png"
+                    alt="more"
+                    className="more_img"
+                  />
+                </div>
+              </div>
+              <div className="user_upload_awards">
+                <img src="img/upload_mp_list.png" alt="mp" />
+              </div>
+            </div>
+            <div className="user_mp">
+              <div className="user_mp_txt">
+                <p>대표작</p>
+                <span>뮤즈의 대표작 포스터를 올려주시면, 매칭확률 UP!</span>
+                <div className="more_con">
+                  <span className="more_more">더보기</span>
+                  <img
+                    src="img/arrow_front.png"
+                    alt="more"
+                    className="more_img"
+                  />
+                </div>
+              </div>
+              <div className="user_upload_awards">
+                <img src="img/upload_mp_list.png" alt="mp" />
+              </div>
+            </div>
+            <div className="user_mp">
+              <div className="user_mp_txt">
+                <p>소셜/웹사이트</p>
+                <span>인스타그램, 유튜브 등 SNS/웹사이트를 등록해주세요.</span>
+                <div className="more_con">
+                  <span className="more_more">더보기</span>
+                  <img
+                    src="img/arrow_front.png"
+                    alt="more"
+                    className="more_img"
+                  />
+                </div>
+              </div>
+              <div className="user_upload_awards">
+                <img src="img/upload_mp_list.png" alt="mp" />
+              </div>
+            </div>
+            <div className="user_mp">
+              <div className="user_mp_txt">
+                <p>영상</p>
+                <span>오디션에 도움이 될 영상이 있다면 올려주세요.</span>
+                <div className="more_con">
+                  <span className="more_more">더보기</span>
+                  <img
+                    src="img/arrow_front.png"
+                    alt="more"
+                    className="more_img"
+                  />
+                </div>
+              </div>
+              <div className="user_upload_awards">
+                <img src="img/upload_mp_list.png" alt="mp" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
